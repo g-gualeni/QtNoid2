7f0281d26b8d13394b1c4f85d71ec3f7608731d8
g-gualeni-c <g.gualeni@gmail.com>
2025-06-03 23:04:21 +0200

    Scale class done and tested
    Doc is the next step

--- Common/include/QtNoidCommon/scale.h
+++ Common/include/QtNoidCommon/scale.h
@@ -13,11 +13,14 @@ class QTNOIDCOMMON_EXPORT Scale
 public:
     static QStringList prefixList(){return {"k","M","G","T","P","E"};} // -> we need 128 bit for this << "Z" << "Y";
     static qint64 multiplier(const QString &prefix);
-    static QString multiplierPrefix(qint64 scale);
+    static QString multiplierPrefix(qint64 value);
     static QString autoUpToExaByte(qint64 value, const QString& unitOfMeasure = "Byte");
     static QString autoUpToExa(qint64 value, const QString& unitOfMeasure);
     static QString nanoSecsUpToDays(quint64 ns);
     static QString nanoSecsUpToDays(qint64 ns);
+
+private:
+    static QString removeBracketsAndSpacesFromUnitOfMeasure(const QString& unitOfMeasure);
 };
 
 } // namespace QtCommon
