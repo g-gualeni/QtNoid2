95d0e92bc04b4cfe769d5e5cfc845ae788e3b91d
g-gualeni-c <g.gualeni@gmail.com>
2025-06-06 23:04:44 +0200

    Working on File. Some errors

--- Common/tests/test_file.cpp
+++ Common/tests/test_file.cpp
@@ -11,16 +11,48 @@ private slots:
     void init();
     void cleanup();
 
-    void autoNamingNextName_data();
-    void autoNamingNextName();
+    void testAutoNamingNextName_data();
+    void testAutoNamingNextName();
 
-    void autoNaming_data();
-    void autoNaming();
+    void testAutoNaming_data();
+    void testAutoNaming();
 
 private:
-    QString testDataPath() const {
-        return QDir::currentPath() + "/TestData/" +
-               metaObject()->className() + "/";
+    QDir testDataDir(const QString &testMethod, const QString &dataTag) const
+    {
+        QString dataPath = QDir::currentPath() +
+                           QDir::separator() + metaObject()->className() +
+                           QDir::separator() + testMethod;
+
+        if (!dataTag.isEmpty()) {
+            dataPath += QDir::separator() + dataTag;
+        }
+
+        return QDir(dataPath);
+    }
+
+    bool testDataDirInit(const QDir &dataDir, const QStringList &fileList) const
+    {
+        auto dir = dataDir;
+        dir.removeRecursively();
+        auto path = dataDir.absolutePath();
+        dir.mkpath(path);
+
+        if(!dir.exists()) {
+            return false;
+        }
+
+        // Create files
+        QFileInfo FI;
+        for(const QString &fName : fileList){
+            FI.setFile(dir, fName);
+            dir.mkpath(FI.path());
+            QFile f(FI.absoluteFilePath());
+            f.open(QIODevice::WriteOnly | QIODevice::Text);
+            f.write(QString(fName + " - test file").toLatin1());
+            f.close();
+        }
+        return true;
     }
 
 };
@@ -40,7 +72,7 @@ void TestFile::init()
 void TestFile::cleanup()
 {}
 
-void TestFile::autoNamingNextName_data()
+void TestFile::testAutoNamingNextName_data()
 {
     QTest::addColumn<QString>("currentName");
     QTest::addColumn<QString>("expected");
@@ -59,7 +91,7 @@ void TestFile::autoNamingNextName_data()
     QTest::newRow("No Extension") << "ReadMe01" << "ReadMe02";
 }
 
-void TestFile::autoNamingNextName()
+void TestFile::testAutoNamingNextName()
 {
     QFETCH(QString, currentName);
     QFETCH(QString, expected);
@@ -67,52 +99,36 @@ void TestFile::autoNamingNextName()
     QCOMPARE(result, expected);
 }
 
-void TestFile::autoNaming_data()
+void TestFile::testAutoNaming_data()
 {
-    QTest::addColumn<QString>("root");
     QTest::addColumn<QStringList>("fileList");
     QTest::addColumn<QString>("newFile");
     QTest::addColumn<QString>("expected");
 
-    // Init the file list
-    QString path = testDataPath() + "autoNaming-";
-
     // Set the test case
-    QTest::newRow("Single File") << path + "Single/" << QStringList({}) << "saturn.txt" << "saturn.txt";
-    QTest::newRow("Single With Index") << path + "Single/" << QStringList({}) << "mars123.txt" << "mars123.txt";
-    QTest::newRow("Duplicated") << path + "Duplicated/" << QStringList({"saturn.txt", "mars.txt"}) << "saturn.txt" << "saturn 001.txt";
-    QTest::newRow("Duplicated With Index") << path + "Duplicated/" << QStringList({"saturn_001.txt", "mars.txt"}) << "saturn_001.txt" << "saturn_002.txt";
-    QTest::newRow("Duplicated Multiple") << path  + "DuplicatedMultiple/" << QStringList({"saturn_001.txt","saturn_002.txt", "saturn_1001.txt"}) << "saturn_001.txt" << "saturn_003.txt";
+    QTest::newRow("Single File") << QStringList({}) << "saturn.txt" << "saturn.txt";
+    QTest::newRow("Single With Index") << QStringList({}) << "mars123.txt" << "mars123.txt";
+    QTest::newRow("Duplicated") << QStringList({"saturn.txt", "mars.txt"}) << "saturn.txt" << "saturn 001.txt";
+    QTest::newRow("Duplicated With Index") << QStringList({"saturn_001.txt", "mars.txt"}) << "saturn_001.txt" << "saturn_002.txt";
+    QTest::newRow("Duplicated Multiple") << QStringList({"saturn_001.txt","saturn_002.txt", "saturn_1001.txt"}) << "saturn_001.txt" << "saturn_003.txt";
 }
 
-void TestFile::autoNaming()
+void TestFile::testAutoNaming()
 {
-    QFETCH(QString, root);
     QFETCH(QStringList, fileList);
     QFETCH(QString, newFile);
     QFETCH(QString, expected);
 
-    // Cleanup
-    QDir dir(root);
-    QCOMPARE(dir.removeRecursively(), true);
-    QCOMPARE(dir.mkpath(root), true);
+    // Init the file list
 
-    // Create folder and files
-    QFileInfo FI;
-    for(const QString &fName : fileList){
-        FI.setFile(dir.absoluteFilePath(fName));
-        QCOMPARE(dir.mkpath(FI.path()), true);
-        QFile f(FI.absoluteFilePath());
-        QCOMPARE(f.open(QIODevice::WriteOnly | QIODevice::Text), true);
-        f.write( "fileInfoAutoNaming - test file");
-        f.close();
-    }
+    QDir dir = testDataDir(__func__, QTest::currentDataTag());
+    auto res = testDataDirInit(dir, fileList);
+    QCOMPARE(res, true);
 
-    // Test Phase
-    auto filePath = dir.absoluteFilePath(newFile);
-    auto result = File::fileAutoNaming(filePath);
+    auto result = File::fileAutoNaming(dir.absoluteFilePath(newFile));
+    QFileInfo FI(result);
 
-    QCOMPARE(result, expected);
+    QCOMPARE(result, dir.absoluteFilePath(expected));
 
 }
 
