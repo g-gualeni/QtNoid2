aa1502f71e25a6105c2807953889a9cd33fd74eb
g-gualeni-c <g.gualeni@gmail.com>
2025-06-08 00:04:59 +0200

    tweaks- new ideas

--- Common/include/QtNoidCommon/file.h
+++ Common/include/QtNoidCommon/file.h
@@ -24,11 +24,14 @@ public:
     static bool isTextFile(const QString &filePath);
     static bool isTextFile(const QFileInfo &fileInfo);
 
-    static qint64 fileSaveAsTextFile(const QString &absoluteFilePath, const QByteArray &data, bool addExtension = true);
-    static qint64 fileSaveAsTextFile(const QString &absoluteFilePath, const QStringList &data, bool addExtension = true);
-    static qint64 fileSaveAsTextFile(const QString &absoluteFilePath, const QString &data, bool addExtension = true);
+    static qint64 saveAsTextFile(const QString &absoluteFilePath, const QByteArray &data, bool addExtension = true);
+    static qint64 saveAsTextFile(const QString &absoluteFilePath, const QStringList &data, bool addExtension = true);
+    static qint64 saveAsTextFile(const QString &absoluteFilePath, const QString &data, bool addExtension = true);
 
-    static QStringList fileLoadAsStringList(const QString &absoluteFilePath);
+    static QStringList readAsStringList(const QString &absoluteFilePath);
+
+    // Make it static, use a private non static
+    QStringList listPathRecursively(const QString &path, const QStringList &nameFilters);
 
 private:
 };
